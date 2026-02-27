817e400742c338dd6a2734ee0d0212f9cb288a30
kevinparker-dev <198780910+kevinparker-dev@users.noreply.github.com>
2026-02-27 14:11:22 +0500

    added google and apple stores link

--- src/components/common/AppleStoreButton.jsx
+++ src/components/common/AppleStoreButton.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import { FaApple } from "react-icons/fa";
 
 import { useTheme } from "@/contexts/ThemeContext";
+import Link from "next/link";
 
 const AppleStoreButton = ({
   varient, // "colored" | "white"
@@ -10,20 +11,25 @@ const AppleStoreButton = ({
 
   return (
     <div className="relative w-fit">
-      <button
-        className={`${
-          varient === "colored"
-            ? `text-white ${isDark ? "bg-[#556B2F]" : "bg-[#7D5B3F]"}`
-            : `bg-white ${colorStyles}`
-        } rounded-full px-3 py-3 sm:px-6 sm:py-3  flex items-center gap-2 text-nowrap text-xs sm:text-base`}
+      <Link
+        href="https://apps.apple.com/us/app/chubbs-army/id6756720892"
+        target="_blank"
       >
-        <FaApple className="text-lg sm:text-2xl" />
-        Download for IPhone
-      </button>
+        <button
+          className={`${
+            varient === "colored"
+              ? `text-white ${isDark ? "bg-[#556B2F]" : "bg-[#7D5B3F]"}`
+              : `bg-white ${colorStyles}`
+          } cursor-pointer rounded-full px-3 py-3 sm:px-6 sm:py-3  flex items-center gap-2 text-nowrap text-xs sm:text-base`}
+        >
+          <FaApple className="text-lg sm:text-2xl" />
+          Download for IPhone
+        </button>
+      </Link>
 
-      <div className="absolute top-[80%] right-0 -rotate-6 text-white bg-gradient-to-r from-[#9E0406] to-[#E48080] rounded-full px-3 py-1 sm:py-2 text-[10px] sm:text-xs">
+      {/* <div className="absolute top-[80%] right-0 -rotate-6 text-white bg-gradient-to-r from-[#9E0406] to-[#E48080] rounded-full px-3 py-1 sm:py-2 text-[10px] sm:text-xs">
         Coming Soon
-      </div>
+      </div> */}
     </div>
   );
 };
--- src/components/common/GooglePlayStoreButton.jsx
+++ src/components/common/GooglePlayStoreButton.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import { FaApple, FaGooglePlay } from "react-icons/fa";
 
 import { useTheme } from "@/contexts/ThemeContext";
+import Link from "next/link";
 
 const GooglePlayStoreButton = ({
   varient, // "colored" | "white"
@@ -10,22 +11,23 @@ const GooglePlayStoreButton = ({
 
   return (
     <div className="relative w-fit">
-      <button
-        className={`bg-transparent rounded-full border-2 ${
-          varient === "colored"
-            ? isDark
-              ? "border-white text-white"
-              : "bg-[#7D5B3F] text-[#7D5B3F]"
-            : "border-white text-white"
-        } px-3 py-3 sm:px-6 sm:py-3 flex items-center gap-2 text-nowrap text-xs sm:text-base`}
-      >
-        <FaGooglePlay className="text-lg sm:text-2xl" />
-        Download for Android
-      </button>
-
-      <div className="absolute top-[80%] right-0 -rotate-6 text-white bg-gradient-to-r from-[#9E0406] to-[#E48080] rounded-full px-3 py-1 sm:py-2 text-[10px] sm:text-xs">
+      <Link href="https://play.google.com/store/apps/details?id=com.cts.chubbsarmy&hl=en" target="_blank">
+        <button
+          className={`bg-transparent rounded-full border-2 ${
+            varient === "colored"
+              ? isDark
+                ? "border-white text-white"
+                : "bg-[#7D5B3F] text-[#7D5B3F]"
+              : "border-white text-white"
+          } cursor-pointer px-3 py-3 sm:px-6 sm:py-3 flex items-center gap-2 text-nowrap text-xs sm:text-base`}
+        >
+          <FaGooglePlay className="text-lg sm:text-2xl" />
+          Download for Android
+        </button>
+      </Link>
+      {/* <div className="absolute top-[80%] right-0 -rotate-6 text-white bg-gradient-to-r from-[#9E0406] to-[#E48080] rounded-full px-3 py-1 sm:py-2 text-[10px] sm:text-xs">
         Coming Soon
-      </div>
+      </div> */}
     </div>
   );
 };
